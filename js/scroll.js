b8469199c4480f9a3311a17af14ebc6aadf1c710
Seungyeol <marudev4905@gmail.com>
2022-02-27 20:50:09 +0900

    '.'

--- js/scroll.js
+++ js/scroll.js
@@ -2,10 +2,16 @@ $(document).ready(function(){
     var header = $("header");
     var body = $("body");
     var winScrollY;
-    
+    var stickyText = $(".sticky-text");
 
     $(window).scroll(function(){
         winScrollY = $(window).scrollTop();
+
+        if(winScrollY >= $(window).innerHeight()){
+            stickyText.addClass("show");
+        }else{
+            stickyText.removeClass("show");
+        }
 
         if(winScrollY >= 100){
             body.addClass("scroll");
